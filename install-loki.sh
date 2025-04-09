c676216ca48e03af63a34173335285411c272c4d
Charan6352 <160250953+Charan6352@users.noreply.github.com>
2025-04-09 10:53:48 +0530

    Update install-loki.sh

--- install-loki.sh
+++ install-loki.sh
@@ -1,6 +1,18 @@
+#!/bin/bash
+
+# Download Loki
 wget https://github.com/grafana/loki/releases/download/v2.9.0/loki-linux-amd64.zip
+
+# Unzip the downloaded file
 unzip loki-linux-amd64.zip
+
+# Move Loki binary to /usr/local/bin
 sudo mv loki-linux-amd64 /usr/local/bin/loki
 
-sudo nano /etc/systemd/system/loki.service
+# Copy loki.service to /etc/systemd/system/
+sudo cp loki.service /etc/systemd/system/loki.service
+
+# Inform the user
+echo "Loki binary installed, and service file copied to /etc/systemd/system/. Please configure the loki.service file as needed."
+
 
