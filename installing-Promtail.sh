8ba20e5b6568e70e9de1a35a4ac274995cb0fb73
Charan6352 <160250953+Charan6352@users.noreply.github.com>
2025-04-09 11:03:01 +0530

    Update installing-Promtail.sh

--- installing-Promtail.sh
+++ installing-Promtail.sh
@@ -1,3 +1,8 @@
 wget https://github.com/grafana/loki/releases/download/v2.9.0/promtail-linux-amd64.zip
 unzip promtail-linux-amd64.zip
 sudo mv promtail-linux-amd64 /usr/local/bin/promtail
+sudo systemctl daemon-reload
+sudo systemctl start promtail
+sudo systemctl enable promtail
+sudo mkdir -p /etc/promtail
+sudo nano /etc/promtail/promtail-config.yaml
